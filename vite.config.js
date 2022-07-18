277dc36f49254d2d7220546e742abd013c147fa2
i74ifa <i74ifa@gmail.com>
2022-07-18 13:53:14 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+const path = require('path');
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-});
+  plugins: [vue()],
+  server: {
+    host: '0.0.0.0'
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@', replacement: path.resolve(__dirname, 'src')
+      }
+    ]
+  }
+})
