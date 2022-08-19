5a15a33e2a5ad58ad91765c85f0a5acc35bfc33f
i74ifa <i74ifa@gmail.com>
2022-08-19 10:23:54 +0300

    favicon

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { plugin as metaPlugin } from 'vue-meta';
 const path = require('path');
 
 // import { createHtmlPlugin } from 'vite-plu'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: './src/main.js'
+    }
+  },
+  plugins: [vue()],  
   server: {
     host: '0.0.0.0'
   },
