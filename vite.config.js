d84cac8828a4296feb1e84f4b74dafbaa73bb629
i74ifa <i74ifa@gmail.com>
2022-08-20 18:07:40 +0300

    fix index not building

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,6 @@ const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: './src/main.js'
-    }
-  },
   plugins: [vue()],  
   server: {
     host: '0.0.0.0'
